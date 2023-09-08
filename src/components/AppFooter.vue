84cabe6b3c4161fe49f1d21348196bce55a52049
StefanoMela <stefano.mela25@gmail.com>
2023-09-08 15:11:23 +0200

    feat: added footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,16 +1,94 @@
 <script>
-
 export default {
-    data () {
-        return {
-
-            links: '',
-        }
-    }
-}
-
+  data() {
+    return {
+      links: "",
+    };
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <div class="footer-container">
+    <div class="fc_link_group">
+        <ul class="fc_first_link_group">
+          <li>First link</li>
+          <li>Second Link</li>
+          <li>Third Link</li>
+          <li>Fourth Link</li>
+        </ul>
+        <ul class="fc_second_link_group">
+          <li>First link</li>
+          <li>Second Link</li>
+          <li>Third Link</li>
+          <li></li>
+        </ul>
+        <ul class="fc_third_link_group">
+          <li>First link</li>
+          <li>Second Link</li>
+          <li>Third Link</li>
+          <li>Fourth Link</li>
+        </ul>
+    </div>
+    <div class="fc_img_overlay_container">
+    </div>
+    <div class="bottom-footer">
+        <div class="fc_btn_container">
+            <button>Call To Action</button>
+        </div>
+        <div class="fc_social_icons_container">
+            <span>FOLLOW US</span>
+            <i>iconeSocial</i>
+            <i>iconeSocial</i>
+            <i>iconeSocial</i>
+            <i>iconeSocial</i>
+            <i>iconeSocial</i>
+        </div>
+    </div>
+  </div>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+.footer-container{
+
+    width: 100%;
+    height: 400px;
+    background-color: rgba(34, 32, 32, 0.915);
+
+    padding-top: 30px;
+
+    .fc_link_group{
+
+        width: 40%;
+
+        padding-left: 30px;
+
+        display: flex;
+        flex-direction: column;
+
+    }
+
+    .fc_img_overlay_container{
+
+        width: 60%;
+    }
+
+    .bottom-footer{
+
+        width: 100%;
+        height: 120px;
+
+        border: 1px solid red;
+
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+
+        .fc_social_icons_container>*{
+
+            padding-inline: 1rem;
+        }
+
+    }
+}
+</style>
--- src/components/AppTopFooter.vue
+++ src/components/AppTopFooter.vue
@@ -11,6 +11,31 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <div class="container-fluid">
+        <i>icona</i>
+        <i>icona</i>
+        <i>icona</i>
+        <i>icona</i>
+    </div>
+</template>
 
-<style></style>
+<styl lang="scss">
+.container-fluid{
+
+    width: 100%;
+    height: 300px;
+    background-color: aqua;
+
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    i {
+
+        background-color: black;
+        color: white;
+        padding: 10px;
+    }
+}
+</styl>

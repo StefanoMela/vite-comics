ca34d9f68027788f9c2ed674ce50fb70cc4456ca
StefanoMela <stefano.mela25@gmail.com>
2023-09-08 13:45:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppTopFooter from "./components/AppTopFooter.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: { AppHeader, AppMain, AppTopFooter, AppFooter },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppTopFooter />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use './styles/reset.scss' as *;
+@use './styles/general.scss' as *;
+</style>
